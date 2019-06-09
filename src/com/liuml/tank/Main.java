61b5fd081ee3ef42a1d603a53232168aa2c9ea60
Liuml <951442843@qq.com>
2019-06-09 12:26:23 +0800

    backup

--- src/com/liuml/tank/Main.java
+++ src/com/liuml/tank/Main.java
@@ -3,8 +3,8 @@ package com.liuml.tank;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        TankFrame tankFrame = new TankFrame();
-        int initTankCount = PropertyMgr.getInstance().getInt("initTankCount");
+        com.liuml.tank.TankFrame tankFrame = new com.liuml.tank.TankFrame();
+        int initTankCount = com.liuml.tank.PropertyMgr.getInstance().getInt("initTankCount");
         for (int i = 0; i < initTankCount; i++) {
             tankFrame.addEnemyTank();
         }
